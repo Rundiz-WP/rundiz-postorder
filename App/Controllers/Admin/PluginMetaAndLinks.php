c156d1c6145c69c74ebbaea906d74a86b7f17bcc
ve3 <kawewong@gmail.com>
2022-01-31 15:53:11 +0700

    Add settings link to network plugin admin.

--- App/Controllers/Admin/PluginMetaAndLinks.php
+++ App/Controllers/Admin/PluginMetaAndLinks.php
@@ -18,20 +18,25 @@ if (!class_exists('\\RdPostOrder\\App\\Controllers\\Admin\\PluginMetaAndLinks'))
         public function actionLinks($actions, $plugin_file)
         {
             static $plugin;
-            
+
             if (!isset($plugin)) {
                 $plugin = plugin_basename(RDPOSTORDER_FILE);
             }
-            
+
             if ($plugin == $plugin_file) {
-                if (current_user_can('manage_options')) {
+                $link = [];
+                if (current_user_can('manage_options') && !is_network_admin()) {
                     $link['settings'] = '<a href="'.  esc_url(get_admin_url(null, 'options-general.php?page=rd-postorder-settings')).'">'.__('Settings').'</a>';
+                    $actions = array_merge($link, $actions);
+                }
+                if (current_user_can('manage_network_plugins') && is_network_admin()) {
+                    $link['networksettings'] = '<a href="'.  esc_url(network_admin_url('settings.php?page=rd-postorder-networksettings')).'">'.__('Settings').'</a>';
                     $actions = array_merge($link, $actions);
                 }
                 //$actions['after_actions'] = '<a href="#" onclick="return false;">'.__('After Actions', 'rd-yte').'</a>';
                 unset($link);
             }
-            
+
             return $actions;
         }// actionLinks
 
@@ -42,7 +47,8 @@ if (!class_exists('\\RdPostOrder\\App\\Controllers\\Admin\\PluginMetaAndLinks'))
         public function registerHooks()
         {
             // add filter action links. this will be displayed in actions area of plugin page. for example: xxxActionLinksBefore | Activate | Edit | Delete | xxxActionLinksAfter
-            add_filter('plugin_action_links', [&$this, 'actionLinks'], 10, 5);
+            add_filter('plugin_action_links_' . plugin_basename(RDPOSTORDER_FILE), [$this, 'actionLinks'], 10, 4);
+            add_filter('network_admin_plugin_action_links_' . plugin_basename(RDPOSTORDER_FILE), [$this, 'actionLinks'], 10, 4);
             // add filter to row meta. (in plugin page below description) Version xx | By xxx | View details | xxxRowMetaxxx | xxxRowMetaxxx
             add_filter('plugin_row_meta', [$this, 'rowMeta'], 10, 2);
         }// registerHooks
@@ -59,17 +65,17 @@ if (!class_exists('\\RdPostOrder\\App\\Controllers\\Admin\\PluginMetaAndLinks'))
         public function rowMeta($links, $file)
         {
             static $plugin;
-            
+
             if (!isset($plugin)) {
                 $plugin = plugin_basename(RDPOSTORDER_FILE);
             }
-            
+
             if ($plugin === $file) {
                 //$new_link[] = '<a href="" target=""></a>';
                 //$links = array_merge($links, $new_link);
                 //unset($new_link);
             }
-            
+
             return $links;
         }// rowMeta
 
